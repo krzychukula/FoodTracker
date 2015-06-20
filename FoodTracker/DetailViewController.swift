dd5cc8f9a268f5b4768970ae7443a59663e87839
krzychukula <krzychukula@gmail.com>
2015-06-20 17:09:16 +0200

    detail view controller

---
+++ FoodTracker/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  FoodTracker
+//
+//  Created by Krzysztof Kula on 20/06/15.
+//  Copyright (c) 2015 Krzysztof Kula. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func eatItBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+    }
+}
