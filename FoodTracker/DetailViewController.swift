b3da484d3957698a2f75931ce59c39925e4f5d7f
krzychukula <krzychukula@gmail.com>
2015-06-21 20:41:00 +0200

    display all the fields

--- FoodTracker/DetailViewController.swift
+++ FoodTracker/DetailViewController.swift
@@ -69,6 +69,78 @@ class DetailViewController: UIViewController {
         
         item.appendAttributedString(titleString)
         
+        
+        
+        var leftParagraphStyle = NSMutableParagraphStyle()
+        leftParagraphStyle.alignment = NSTextAlignment.Left
+        leftParagraphStyle.lineSpacing = 20.0
+        
+        var firstWordDictionary = [
+            NSForegroundColorAttributeName :  UIColor.blackColor(),
+            NSFontAttributeName : UIFont.boldSystemFontOfSize(18.0),
+            NSParagraphStyleAttributeName: leftParagraphStyle
+        ]
+        var style1Dictionary = [
+            NSForegroundColorAttributeName :  UIColor.darkGrayColor(),
+            NSFontAttributeName : UIFont.systemFontOfSize(18.0),
+            NSParagraphStyleAttributeName: leftParagraphStyle
+        ]
+        var style2Dictionary = [
+            NSForegroundColorAttributeName :  UIColor.lightGrayColor(),
+            NSFontAttributeName : UIFont.systemFontOfSize(18.0),
+            NSParagraphStyleAttributeName: leftParagraphStyle
+        ]
+        
+        
+        let calciumTitleString = NSAttributedString(string: "Calcium ", attributes: firstWordDictionary);
+        let calciumBodyString = NSAttributedString(string: "\(usdaItem.calcium)% \n", attributes: style1Dictionary)
+        item.appendAttributedString(calciumTitleString)
+        item.appendAttributedString(calciumBodyString)
+        
+        
+        let carbohydrateTitleString = NSAttributedString(string: "Carbohydrate ", attributes: firstWordDictionary);
+        let carbohydrateBodyString = NSAttributedString(string: "\(usdaItem.carbohydrate)% \n", attributes: style2Dictionary)
+        item.appendAttributedString(carbohydrateTitleString)
+        item.appendAttributedString(carbohydrateBodyString)
+        
+        
+        let cholesterolTitleString = NSAttributedString(string: "Cholesterol ", attributes: firstWordDictionary);
+        let cholesterolBodyString = NSAttributedString(string: "\(usdaItem.cholesterol)% \n", attributes: style1Dictionary)
+        item.appendAttributedString(cholesterolTitleString)
+        item.appendAttributedString(cholesterolBodyString)
+        
+        
+        // Energy
+        let energyTitleString = NSAttributedString(string: "Energy ", attributes: firstWordDictionary)
+        let energyBodyString = NSAttributedString(string: "\(usdaItem.energy)% \n", attributes: style2Dictionary)
+        item.appendAttributedString(energyTitleString)
+        item.appendAttributedString(energyBodyString)
+        
+        // Fat Total
+        let fatTotalTitleString = NSAttributedString(string: "FatTotal ", attributes: firstWordDictionary)
+        let fatTotalBodyString = NSAttributedString(string: "\(usdaItem.fatTotal)% \n", attributes: style1Dictionary)
+        item.appendAttributedString(fatTotalTitleString)
+        item.appendAttributedString(fatTotalBodyString)
+        
+        // Protein
+        let proteinTitleString = NSAttributedString(string: "Protein ", attributes: firstWordDictionary)
+        let proteinBodyString = NSAttributedString(string: "\(usdaItem.protein)% \n", attributes: style2Dictionary)
+        item.appendAttributedString(proteinTitleString)
+        item.appendAttributedString(proteinBodyString)
+        
+        
+        // Sugar
+        let sugarTitleString = NSAttributedString(string: "Sugar ", attributes: firstWordDictionary)
+        let sugarBodyString = NSAttributedString(string: "\(usdaItem.sugar)% \n", attributes: style1Dictionary)
+        item.appendAttributedString(sugarTitleString)
+        item.appendAttributedString(sugarBodyString)
+        
+        // Vitamin C
+        let vitaminCTitleString = NSAttributedString(string: "Vitamin C ", attributes: firstWordDictionary)
+        let vitaminCBodyString = NSAttributedString(string: "\(usdaItem.vitaminC)% \n", attributes: style2Dictionary)
+        item.appendAttributedString(vitaminCTitleString)
+        item.appendAttributedString(vitaminCBodyString)
+        
         return item
     }
 }
